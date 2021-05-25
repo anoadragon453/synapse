8a73c4500186b6b1536c019bf0a7adca8b242fe1
anoadragon453 <anoadragon453@users.noreply.github.com>
2021-05-25 22:42:26 +0000

    deploy: 827ec79b8e4a106e75d43cd9b28c64ba27ad1f74

--- latest/docs-mdbook/book-assets/sidebar.js
+++ latest/docs-mdbook/book-assets/sidebar.js
@@ -1,71 +1,131 @@
-// Un-active everything when you click it
-Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
-    el.addEventHandler("click", function() {
-        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
-            el.classList.remove("active");
+const getPageToc = () => document.getElementsByClassName('pagetoc')[0];
+
+const pageToc = getPageToc();
+const pageTocChildren = [...pageToc.children];
+const headers = [...document.getElementsByClassName('header')];
+
+
+// Select highlighted item in ToC when clicking an item
+pageTocChildren.forEach(child => {
+    child.addEventHandler('click', () => {
+        pageTocChildren.forEach(child => {
+            child.classList.remove('active');
         });
-        el.classList.add("active");
+        child.classList.add('active');
     });
 });
 
-var updateFunction = function() {
-    var id;
-    var elements = document.getElementsByClassName("header");
-    Array.prototype.forEach.call(elements, function(el, i) {
-        if (window.pageYOffset >= el.offsetTop) {
-            id = el;
+
+/**
+ * Test whether a node is in the viewport
+ */
+function isInViewport(node) {
+    const rect = node.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
+
+/**
+ * Set a new ToC entry.
+ * Clear any previously highlighted ToC items, set the new one,
+ * and adjust the ToC scroll position.
+ */
+function setTocEntry() {
+    let activeEntry;
+    const pageTocChildren = [...getPageToc().children];
+
+    // Calculate which header is the current one at the top of screen
+    headers.forEach(header => {
+        if (window.pageYOffset >= header.offsetTop) {
+            activeEntry = header;
         }
     });
 
-    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
-        el.classList.remove("active");
-    });
-
-    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el, i) {
-        if (id.href.localeCompare(el.href) == 0) {
-            el.classList.add("active");
+    // Update selected item in ToC when scrolling
+    pageTocChildren.forEach(child => {
+        if (activeEntry.href.localeCompare(child.href) === 0) {
+            child.classList.add('active');
+        } else {
+            child.classList.remove('active');
         }
     });
-};
 
-// Populate sidebar on load
-window.addEventListener('load', function() {
-    var pagetoc = document.getElementsByClassName("pagetoc")[0];
-    var elements = document.getElementsByClassName("header");
-    Array.prototype.forEach.call(elements, function(el, i) {
-        var link = document.createElement("a");
-
-        // Indent shows hierarchy
-        var indent = "";
-        switch (el.parentElement.tagName) {
-            case "H2":
-                indent = "20px";
-                break;
-            case "H3":
-                indent = "30px";
-                break;
-            case "H4":
-                indent = "40px";
-                break;
-            case "H5":
-                indent = "50px";
-                break;
-            case "H6":
-                indent = "60px";
-                break;
-            default:
-                break;
+    let tocEntryForLocation = document.querySelector(`nav a[href="${activeEntry.href}"]`);
+    if (tocEntryForLocation) {
+        const headingForLocation = document.querySelector(activeEntry.hash);
+        if (headingForLocation && isInViewport(headingForLocation)) {
+            // Update ToC scroll
+            const nav = getPageToc();
+            const content = document.querySelector('html');
+            if (content.scrollTop !== 0) {
+                nav.scrollTo({
+                    top: tocEntryForLocation.offsetTop - 100,
+                    left: 0,
+                    behavior: 'smooth',
+                });
+            } else {
+                nav.scrollTop = 0;
+            }
         }
+    }
+}
 
-        link.appendChild(document.createTextNode(el.text));
-        link.style.paddingLeft = indent;
-        link.href = el.href;
-        pagetoc.appendChild(link);
-    });
-    updateFunction.call();
+
+/**
+ * Populate sidebar on load
+ */
+window.addEventListener('load', () => {
+    // Only create table of contents if there is more than one header on the page
+    if (headers.length > 1) {
+        headers.forEach((header, index) => {
+            const link = document.createElement('a');
+
+            // Indent shows hierarchy
+            let indent = '0px';
+            switch (header.parentElement.tagName) {
+                case 'H1':
+                    indent = '5px';
+                    break;
+                case 'H2':
+                    indent = '20px';
+                    break;
+                case 'H3':
+                    indent = '30px';
+                    break;
+                case 'H4':
+                    indent = '40px';
+                    break;
+                case 'H5':
+                    indent = '50px';
+                    break;
+                case 'H6':
+                    indent = '60px';
+                    break;
+                default:
+                    break;
+            }
+
+            let tocEntry;
+            if (index == 0) {
+                // Create a bolded title for the first element
+                tocEntry = document.createElement("strong");
+                tocEntry.innerHTML = header.text;
+            } else {
+                // All other elements are non-bold
+                tocEntry = document.createTextNode(header.text);
+            }
+            link.appendChild(tocEntry);
+
+            link.style.paddingLeft = indent;
+            link.href = header.href;
+            pageToc.appendChild(link);
+        });
+        setTocEntry.call();
+    }
 });
 
 
-
-// Handle active elements on scroll
-window.addEventListener("scroll", updateFunction);
+// Handle active headers on scroll, if there is more than one header on the page
+if (headers.length > 1) {
+    window.addEventListener('scroll', setTocEntry);
+}
